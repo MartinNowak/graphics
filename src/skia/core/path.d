a388048ec43dd7c767f48dc309d4c5dfc6c9f1e9
dawg <dawg@dawgfoto.de>
2010-11-29 02:07:40 +0100

    skia.core.path implementation.
    - Supports only primitve line/quad/cubic move functions.

--- src/skia/core/path.d
+++ src/skia/core/path.d
@@ -4,179 +4,258 @@ import skia.core.point;
 import skia.core.rect;
 
 import std.array;
+import std.conv : to;
+import std.range;
+import std.traits;
 
-debug=WHITEBOX;
+//debug=WHITEBOX;
+debug import std.stdio : writeln, printf;
 
 // TODO FPoint
-class Path
+struct Path
 {
+private:
   IPoint[] points;
-  Verb[] verbs;
-  IRect bounds;
+
+  // FIXME Verb[] breaks std.array.front in const function
+  ubyte[] verbs;
+
+  IRect _bounds;
   bool boundsIsDirty;
   ubyte fillType;
   ubyte isConvex;
 
+public:
+  string toString() const {
+    string res;
+    res ~= "Path, bounds: " ~ to!string(this._bounds) ~ "\n";
+    this.forEach((Verb verb, const IPoint[] pts) {
+        res ~= verbToString(verb) ~ ": ";
+        foreach(IPoint pt; pts) {
+          res ~= to!string(pt) ~ ", ";
+        }
+        res ~= "\n";
+      });
+    return res;
+  }
+  static string verbToString(Verb verb) {
+    final switch(verb) {
+    case Verb.Move: return "Move";
+    case Verb.Line: return "Line";
+    case Verb.Quad: return "Quad";
+    case Verb.Cubic: return "Cubic";
+    case Verb.Close: return "Close";
+    }
+  }
   @property bool empty() const {
     return this.verbs.length == 0
       || this.verbs.length == 1 && this.verbs[0] == Verb.Move;
   }
   @property bool inverseFillType() const { return (this.fillType & 2) != 0; }
+  void toggleInverseFillType() {
+    this.fillType ^= 2;
+  }
+
+  @property IRect bounds() const {
+    return this._bounds;
+  }
+  @property void bounds(in IRect bounds) {
+    return this._bounds = bounds;
+  }
+
   enum Verb : ubyte
   {
-    Move  = 0,
+    Move = 0,
     Line  = 1,
     Quad  = 2,
     Cubic = 3,
     Close,
-    Done,
   }
-  enum NeedMoveToState
-  {
-    AfterClose,
-    AfterCons,
-    AfterPrefix,
+
+  static ubyte verbToMaxEdges(Verb verb) {
+    /*
+     * 0,  //  Verb.Move
+     * 1,  //  Verb.Live
+     * 2,  //  Verb.Quad
+     * 3,  //  Verb.Cubic
+     * 0,  //  Verb.Close
+     */
+    return (verb > 3) ? 0 : verb;
   }
-  struct Iter {
-    IPoint[] points;
-    Verb[] verbs;
-    IPoint moveTo;
+
+  //  alias void delegate(const Verb, const IPoint[]) IterDg;
+  void forEach(IterDg)(IterDg dg) const {
     IPoint lastPt;
-    bool forceClose;
-    // TODO initialization
-    bool needClose;
-    NeedMoveToState needMoveTo;
-    bool closeLine;
+    IPoint moveTo;
 
-    debug(WHITEBOX) private auto opDispatch(string m, Args...)(Args a) {
-      throw new Exception("Unimplemented property "~m);
-    }
+    auto vs = this.verbs.save();
+    auto points = this.points.save();
 
-    this(Path path, bool forceClose) {
-      assert(!(path is null));
-      this.verbs = path.verbs.save();
-      this.points = path.points.save();
-      this.forceClose = forceClose;
-    }
-
-    bool isClosedContour() {
-      if (this.forceClose)
-	return true;
-      
-      auto r = this.verbs.save();
-
-      if (r.front == Verb.Move)
-	r.popFront();
-
-      while(!r.empty) {
-	if (r.front == Verb.Move)
-	  break;
-	if (r.front == Verb.Close)
-	  return true;
-	r.popFront();
-      }
-      return false;
-    }
-
-
-    Verb next(out IPoint[4] pts) {     
-      if (this.verbs.length == 0) {
-	if (this.needClose) {
-	  if (this.autoClose(pts) == Verb.Line)
-	    return Verb.Line;
-	  this.needClose = false;
-	  return Verb.Close;
-	}
-	return Verb.Done;
-      }
-
-
-      Verb verb = this.verbs.front; this.verbs.popFront();
+    while (!vs.empty) {
+      Verb verb = cast(Verb)vs.front; vs.popFront();
 
       final switch (verb) {
-
       case Verb.Move:
-	if (this.needClose) {
-	  this.verbs = this.verbs[-1..$];
-	  verb = this.autoClose(pts);
-	  if (verb == Verb.Close) {
-	    this.needClose = false;
-	  }
-	  return verb;
-	}
-	if (this.verbs.length == 0) {
-	  return Verb.Done;
-	}
-	this.moveTo = this.points[0];
-	pts[0] = this.points.front;
-	this.points.popFront();
-	this.needMoveTo = NeedMoveToState.AfterCons;
-	this.needClose = this.forceClose;
+	dg(Verb.Move, [points.front]);
+        moveTo = points.front;
+	lastPt = points.front; points.popFront();
 	break;
 
-      case Verb.Line, Verb.Quad, Verb.Cubic : {
-	if (this.consMoveTo(pts))
-	  return Verb.Move;
+      case Verb.Line, Verb.Quad, Verb.Cubic:
+	dg(verb, [lastPt] ~ take(points, verb));
 
-	auto nPoints = verb;
-	pts[1..1 + nPoints] = this.points[0..nPoints];
-	this.lastPt = this.points[nPoints-1];
-
-	if (verb == Verb.Line)
-	  this.closeLine = false;
-
-	while (nPoints-- > 0)
-	  this.points.popFront();
-
+	auto popped = popFrontN(points, verb - 1);
+	assert(popped == verb - 1);
+	lastPt = points.front; points.popFront();
 	break;
-      }
 
       case Verb.Close:
-	verb = this.autoClose(pts);
-	if (verb == Verb.Line)
-	  this.verbs = this.verbs[-1..$];
-	else
-	  this.needClose = false;
-	this.needMoveTo = NeedMoveToState.AfterClose;
-	break;
+        if (lastPt != moveTo)
+        {
+          dg(Verb.Line, [lastPt, moveTo]);
+          lastPt = moveTo;
+        }
+        dg(Verb.Close, []);
       }
-      return verb;
     }
+  }
 
-    bool consMoveTo(out IPoint[4] pts) {
-      if (this.needMoveTo == NeedMoveToState.AfterClose) {
-	pts[0] = this.moveTo;
-	this.needClose = this.forceClose;
-	this.needMoveTo = NeedMoveToState.AfterCons;
-	this.verbs = this.verbs[-1..$];
-	return true;
-      }
+  bool isClosedContour() {
+    auto r = this.verbs.save();
 
-      if (this.needMoveTo == NeedMoveToState.AfterCons) {
-	pts[0] = this.moveTo;
-	this.needMoveTo = NeedMoveToState.AfterPrefix;
-      }
-      else {
-	assert(this.needMoveTo == NeedMoveToState.AfterPrefix);
-	pts[0] = this.points[-1];
-      }
-      return false;
+    if (r.front == Verb.Move)
+      r.popFront();
+
+    while(!r.empty) {
+      if (r.front == Verb.Move)
+        break;
+      if (r.front == Verb.Close)
+        return true;
+      r.popFront();
     }
+    return false;
+  }
 
-    Verb autoClose(out IPoint[4] pts) {
-      if (this.lastPt != this.moveTo) {
-	//! TODO: Floating point equality of NAN Points
-	pts[] = [this.lastPt, this.moveTo];
-	this.lastPt = this.moveTo;
-	this.closeLine = true;
-	return Verb.Line;
-      }
-      return Verb.Close;
+  @property IPoint lastPoint() const {
+    return this.points.length == 0 ? IPoint() : this.points[$];
+  }
+
+  bool lastVerbWas(Verb verb) const {
+    return this.verbs.length == 0 ? false : this.verbs[$] == verb;
+  }
+
+  void ensureStart() {
+    if (this.verbs.empty) {
+      assert(this.points.length == 0);
+      this.points ~= IPoint.init;
+      this.verbs ~= Verb.Move;
     }
+  }
+
+  void primTo(IPoint[] pts) {
+    this.ensureStart();
+    this.points ~= pts;
+    this.verbs ~= cast(Verb)pts.length;
+  }
+
+  void rPrimTo(IPoint[] pts) {
+    auto lPt = this.lastPoint;
+    foreach(ref pt; pts) {
+      pt = pt + lPt;
+    }
+    IPoint pt;
+    this.primTo(pts);
+  }
+
+  void moveTo(in IPoint pt) {
+    if (this.lastVerbWas(Verb.Move)) {
+      this.points[$] = pt;
+    }
+    else {
+      this.points ~= pt;
+      this.verbs ~= Verb.Move;
+    }
+    this.boundsIsDirty = true;
+  }
+  void rMoveTo(in IPoint pt) {
+    this.moveTo(this.lastPoint + pt);
+  }
+
+  void lineTo(in IPoint pt) {
+    this.primTo([pt]);
+  }
+  void rLineTo(in IPoint pt) {
+    this.rPrimTo([pt]);
+  }
+
+  void quadTo(in IPoint pt1, in IPoint pt2) {
+    this.primTo([pt1, pt2]);
+  }
+  void rQuadTo(in IPoint pt1, in IPoint pt2) {
+    this.rPrimTo([pt1, pt2]);
+  }
+
+  void cubicTo(in IPoint pt1, in IPoint pt2, in IPoint pt3) {
+    this.primTo([pt1, pt2, pt3]);
+  }
+  void rCubicTo(in IPoint pt1, in IPoint pt2, in IPoint pt3) {
+    this.rPrimTo([pt1, pt2, pt3]);
+  }
+
+  void close() {
+    if (this.verbs.length > 0) {
+      switch (this.verbs[$]) {
+      case Verb.Line, Verb.Quad, Verb.Cubic:
+        this.verbs ~= Verb.Close;
+      default:
+        assert(false);
+        break;
+      }
+    }
+  }
+
+  void computeBounds() {
+    this._bounds = IRect.calcBounds(this.points);
+    this.boundsIsDirty = false;
   }
 
   debug(WHITEBOX) private auto opDispatch(string m, Args...)(Args a) {
     throw new Exception("Unimplemented property "~m);
+  }
+
+  unittest
+  {
+    Path p;
+    p.verbs ~= Verb.Move;
+    p.points ~= IPoint(1, 1);
+    p.verbs ~= Verb.Line;
+    p.points ~= IPoint(1, 3);
+    p.verbs ~= Verb.Quad;
+    p.points ~= [IPoint(2, 4), IPoint(3, 3)];
+    p.verbs ~= Verb.Cubic;
+    p.points ~= [IPoint(4, 2), IPoint(2, -1), IPoint(0, 0)];
+    p.verbs ~= Verb.Close;
+
+    Verb[] verbExp = [Verb.Move, Verb.Line, Verb.Quad, Verb.Cubic, Verb.Line, Verb.Close];
+    IPoint[][] ptsExp = [
+      [IPoint(1,1)],
+      [IPoint(1,1), IPoint(1,3)],
+      [IPoint(1,3), IPoint(2,4), IPoint(3,3)],
+      [IPoint(3,3), IPoint(4,2), IPoint(2,-1), IPoint(0,0)],
+      [IPoint(0,0), IPoint(1,1)],
+      [],
+			 ];
+
+    void iterate(const Verb verb, const IPoint[] pts) {
+      assert(verb == verbExp[0]);
+      assert(pts == ptsExp[0]);
+      verbExp.popFront();
+      ptsExp.popFront();
+    }
+    p.forEach(&iterate);
+
+    assert(p.isClosedContour() == true);
+    assert(p.empty() == false);
   }
 
 }
