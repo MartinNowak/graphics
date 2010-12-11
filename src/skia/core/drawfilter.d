f88aadcf239fdf405ac3f47a23a08a6f31f82756
dawg <dawg@dawgfoto.de>
2010-12-11 19:08:14 +0100

    Drawfilter/Drawlooper interfaces.
    - Can be attached to a canvas/paint and allows interception of drawing
      to manipulate the canvas/paint or filter the drawn output.

---
+++ src/skia/core/drawfilter.d
@@ -0,0 +1,27 @@
+module skia.core.drawfilter;
+
+private {
+  import skia.core.canvas;
+  import skia.core.paint;
+}
+
+interface DrawFilter {
+  enum Type {
+    Paint,
+    Point,
+    Line,
+    Bitmap,
+    Rect,
+    Path,
+    Text,
+  }
+  /** Return true to allow the draw to continue (with possible
+      modified canvas/paint). If true is returned, then restore will
+      be called.
+   */
+  bool filter(Canvas canvas, Paint paint, Type);
+  /** If filter() returned true, then restore will be called to
+      restore the canvas/paint to their previous state.
+   */
+  void restore(Canvas canvas, Paint paint, Type);
+}
