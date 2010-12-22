ec3b8162be72e30cf9fa98a76c9daba65e8fc007
dawg <dawg@dawgfoto.de>
2010-12-22 21:21:53 +0100

    Raise accuracy tolerance.

--- src/QuickCheck/matrix.d
+++ src/QuickCheck/matrix.d
@@ -17,10 +17,6 @@ private string formatString(TL...)(string fmt, TL tl) {
   return writer.data;
 }
 
-
-unittest {
-  doRun();
-}
 FPoint Multiply(in Matrix m, FPoint pt) {
   auto xr = pt.x * m[0][0] + pt.y * m[0][1] + m[0][2];
   auto yr = pt.x * m[1][0] + pt.y * m[1][1] + m[1][2];
@@ -33,15 +29,32 @@ FPoint Multiply(in Matrix m, FPoint pt) {
 }
 
 void doRun() {
-  Matrix m;
-  m.setRotate(45);
-  auto pts = getArbitrary!(FPoint[], size(20), minValue(0.0), maxValue(2.0), Policies.RandomizeMembers)();
+  auto pts = getArbitrary!(FPoint[], size(1000), minValue(0.0), maxValue(2.0), Policies.RandomizeMembers)();
   auto ptsB = pts.idup;
+  Matrix m;
 
-  m.mapPoints(pts);
-  foreach(i, pt; ptsB) {
-    auto mpt = Multiply(m, pt);
-    assert(abs(mpt.x - pts[i].x) < 2*float.epsilon, formatString("unequal pts SSEM:%s FPUM:%s", pts[i], mpt));
-    assert(abs(mpt.y - pts[i].y) < 2*float.epsilon, formatString("unequal pts SSEM:%s FPUM:%s", pts[i], mpt));
+  void assertPointsEquality() {
+    foreach(i, pt; ptsB) {
+      auto mpt = Multiply(m, pt);
+      assert(abs(mpt.x - pts[i].x) < 10*float.epsilon, formatString("unequal pts SSEM:%s FPUM:%s", pts[i], mpt));
+      assert(abs(mpt.y - pts[i].y) < 10*float.epsilon, formatString("unequal pts SSEM:%s FPUM:%s", pts[i], mpt));
+    }
   }
+  m.setRotate(45);
+  m.mapPoints(pts);
+  assertPointsEquality();
+
+  pts = ptsB.dup;
+  m.setTranslate(100, 100);
+  m.mapPoints(pts);
+  assertPointsEquality();
+
+  pts = ptsB.dup;
+  m.setScale(float.min_normal, float.min_normal);
+  m.mapPoints(pts);
+  assertPointsEquality();
+}
+
+unittest {
+  doRun();
 }
