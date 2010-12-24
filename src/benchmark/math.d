d2f8f53b96f4e6a96d2a11fc81899dbcb5193351
dawg <dawg@dawgfoto.de>
2010-12-24 18:50:31 +0100

    Added load m128 benchmark.

--- src/benchmark/math.d
+++ src/benchmark/math.d
@@ -1,8 +1,9 @@
-module benchmark.math;
+module benchmark.skia_math;
 
 private  {
   import benchmark._;
   import skia.core.edgebuilder : fastSqrt;
+  import skia.math.m128;
 }
 
 static this() {
@@ -16,6 +17,14 @@ void benchFastSqrt() {
   }
 }
 
+void loadm128() {
+  m128 var;
+  for (auto i = 0; i < 1_000_000; ++i) {
+    var = m128(i, i, i, i);
+  }
+}
+
 void runMath(BenchmarkReporter reporter) {
+  reporter.bench!(loadm128)();
   reporter.bench!(benchFastSqrt)();
 }
