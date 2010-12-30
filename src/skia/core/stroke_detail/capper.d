03feb40b71167a0bd9a371bcacb120d563d859de
dawg <dawg@dawgfoto.de>
2010-12-30 02:23:50 +0100

    Implemented joiners/cappers.
    - Miter, Round, Bevel join.
    - Square, Round, Butt cap.

--- src/skia/core/stroke_detail/capper.d
+++ src/skia/core/stroke_detail/capper.d
@@ -2,12 +2,35 @@ module skia.core.stroke_detail.capper;
 
 private {
   import skia.core.paint;
+  import skia.core.path;
+  import skia.core.point;
 }
-alias void function() Capper;
+alias void function(FPoint pt, FVector normal, ref Path path) Capper;
 Capper getCapper(Paint.Cap capStyle) {
-  return &NullCapper;
+  final switch(capStyle) {
+  case Paint.Cap.Butt:
+    return &ButtCapper;
+  case Paint.Cap.Square:
+    return &SquareCapper;
+  case Paint.Cap.Round:
+    return &RoundCapper;
+  }
 }
 
-void NullCapper() {}
+void ButtCapper(FPoint pt, FVector normal, ref Path path) {
+  // assert(path.lastPoint + normal == pt, to!string(path.lastPoint) ~ to!string(pt) ~ to!string(normal));
+  path.lineTo(pt + normal);
+}
 
+void RoundCapper(FPoint pt, FVector normal, ref Path path) {
+  // assert(path.lastPoint + normal == pt, to!string(path.lastPoint) ~ to!string(pt) ~ to!string(normal));
+  path.arcTo(pt, pt + normal, Path.Direction.CCW);
+}
 
+void SquareCapper(FPoint pt, FVector normal, ref Path path) {
+  FVector parallel = normal;
+  parallel.rotateCW();
+  path.lineTo(pt - normal + parallel);
+  path.lineTo(pt + normal + parallel);
+  path.lineTo(pt + normal);
+}
--- src/skia/core/stroke_detail/joiner.d
+++ src/skia/core/stroke_detail/joiner.d
@@ -1,11 +1,64 @@
 module skia.core.stroke_detail.joiner;
 
 private {
+  import std.math : abs, sin, asin, PI;
+
   import skia.core.paint;
+  import skia.core.path;
+  import skia.core.point;
 }
-alias void function() Joiner;
+alias void function(FPoint pt, FVector normalBefore,
+                    FVector normalAfter, ref Path inner, ref Path outer) Joiner;
+
 Joiner getJoiner(Paint.Join joinStyle) {
-  return &NullJoiner;
+  final switch(joinStyle) {
+  case Paint.Join.Miter:
+    return &MiterJoiner;
+  case Paint.Join.Round:
+    return &RoundJoiner;
+  case Paint.Join.Bevel:
+    return &BevelJoiner;
+  }
 }
 
-void NullJoiner() {}
+void BevelJoiner(FPoint pt, FVector normalBefore, FVector normalAfter, ref Path inner, ref Path outer) {
+  // assert(outer.lastPoint - normalBefore == pt);
+  outer.lineTo(pt + normalAfter);
+  inner.lineTo(pt - normalAfter);
+ }
+
+void RoundJoiner(FPoint pt, FVector normalBefore, FVector normalAfter, ref Path inner, ref Path outer) {
+  if (clockwise(normalBefore, normalAfter)) {
+    inner.arcTo(pt, pt - normalAfter, Path.Direction.CW);
+    outer.lineTo(pt + normalAfter);
+  } else {
+    outer.arcTo(pt, pt + normalAfter, Path.Direction.CCW);
+    inner.lineTo(pt - normalAfter);
+  }
+}
+
+void MiterJoiner(FPoint pt, FVector normalBefore, FVector normalAfter, ref Path inner, ref Path outer) {
+  auto crossP = crossProduct(normalBefore, normalAfter);
+  auto radius = normalBefore.length;
+  //  assert(normalAfter.length == radius);
+  enum miterLimit = sin(2*PI*15/360);
+  if (abs(crossP) <  miterLimit * radius * radius) {
+    BevelJoiner(pt, normalBefore, normalAfter, inner, outer);
+  } else {
+    auto angle = asin(crossP / (radius * radius));
+    auto midLength = radius / sin(angle * 0.5);
+    auto mid = -normalBefore - normalAfter;
+    mid.setLength(midLength);
+    if (crossP > 0) { // clockwise (see below), but save recalculation
+      inner.lineTo(pt + mid);
+    } else {
+      outer.lineTo(pt + mid);
+    }
+    outer.lineTo(pt + normalAfter);
+    inner.lineTo(pt - normalAfter);
+  }
+}
+
+bool clockwise(FVector before, FVector after) {
+  return crossProduct(before, after) > 0;
+}
