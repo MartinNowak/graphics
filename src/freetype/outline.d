0f8b80b8f55f267245539a635c8c0a8140a34d2b
dawg <dawg@dawgfoto.de>
2011-01-16 22:27:40 +0100

    Added freetype header.

---
+++ src/freetype/outline.d
@@ -0,0 +1,34 @@
+module freetype.outline;
+
+private {
+  import freetype.freetype;
+}
+
+extern(C):
+
+alias int function(const FT_Vector* to, void* user) FT_Outline_MoveToFunc;
+alias int function(const FT_Vector* to, void* user) FT_Outline_LineToFunc;
+
+alias int function(const FT_Vector* control,
+                   const FT_Vector* to,
+                   void* user) FT_Outline_ConicToFunc;
+
+alias int function(const FT_Vector*  control1,
+                   const FT_Vector*  control2,
+                   const FT_Vector*  to,
+                   void* user) FT_Outline_CubicToFunc;
+
+struct FT_Outline_Funcs
+{
+  FT_Outline_MoveToFunc   move_to;
+  FT_Outline_LineToFunc   line_to;
+  FT_Outline_ConicToFunc  conic_to;
+  FT_Outline_CubicToFunc  cubic_to;
+
+  int                     shift;
+  FT_Pos                  delta;
+};
+
+FT_Error FT_Outline_Decompose(FT_Outline* outline,
+                              const FT_Outline_Funcs* func_interface,
+                              void* user);
