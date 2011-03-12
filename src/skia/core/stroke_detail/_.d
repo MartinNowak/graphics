8b76f3f7ff9811c73efb08fe6700f4d1a30f4aa5
dawg <dawg@dawgfoto.de>
2011-03-12 16:05:57 +0100

    public import needed

--- src/skia/core/stroke_detail/_.d
+++ src/skia/core/stroke_detail/_.d
@@ -1,4 +1,6 @@
 module skia.core.stroke_detail._;
 
-import skia.core.stroke_detail.capper : Capper, getCapper;
-import skia.core.stroke_detail.joiner : Joiner, getJoiner;
+public {
+  import skia.core.stroke_detail.capper : Capper, getCapper;
+  import skia.core.stroke_detail.joiner : Joiner, getJoiner;
+}
