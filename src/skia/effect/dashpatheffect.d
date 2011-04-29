0618f3e091f94134a6d738f44e411815cb9742da
dawg <dawg@dawgfoto.de>
2011-04-29 11:16:14 +0200

    first draft of dash path effect

---
+++ src/skia/effect/dashpatheffect.d
@@ -0,0 +1,37 @@
+module skia.effect.dashpatheffect;
+
+import std.algorithm, std.math;
+import skia.core.path, skia.core.patheffect, skia.core.path_detail.path_measure,
+  skia.math.fixed_ary, skia.core.edge_detail.algo;
+import guip.point;
+
+class DashPathEffect : PathEffect {
+  this(float[] intervals) {
+    this.intervals = intervals;
+    this.intervalLength = reduce!("a+b")(0.0f, intervals);
+    this.scaleToFit = scaleToFit;
+  }
+
+  Path filterPath(Path path, ref float width) const {
+    auto meas = PathMeasure(path);
+
+    auto scale = intervalLength > meas.length
+      ? meas.length / intervalLength
+      : meas.length / (floor(meas.length / intervalLength) * intervalLength);
+
+    Path result;
+    auto dist = 0.0;
+    while (dist < meas.length) {
+      foreach(i, dash; intervals) {
+        if ((i & 0x1) == 0)
+          meas.appendRangeToPath(dist, dist + dash, result);
+        dist += dash * scale;
+      }
+    }
+    return result;
+  }
+
+  float[] intervals;
+  float intervalLength;
+  bool scaleToFit;
+}
