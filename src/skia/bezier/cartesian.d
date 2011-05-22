ec75eb00278923d3f453fafcf26f35040b31d898
dawg <dawg@dawgfoto.de>
2011-05-22 10:49:42 +0200

    separate bezier walking from clipping

--- src/skia/bezier/cartesian.d
+++ src/skia/bezier/cartesian.d
@@ -162,41 +162,45 @@ in {
   auto monocnt = clipBezier(curve, clip, monos);
 
   foreach(ref mono; monos[0 .. monocnt]) {
-    auto xwalk = beziota!("x")(mono, grid.width);
-    auto ywalk = beziota!("y")(mono, grid.height);
-    auto slicer = rollingSlicer(mono, 0.0);
-    auto gridPos = IPoint(xwalk.position, ywalk.position);
+    walkMonoBezier!(T, K)(mono, grid, clientDg);
+  }
+}
 
-    for (bool cont=true; cont;) {
-      double nextT;
-      int xadv, yadv;
-      if (!xwalk.empty && !ywalk.empty) {
-        if (approxEqual(xwalk.front, ywalk.front, 1e-6, 1e-6)) {
-          nextT = 0.5 * (xwalk.front + ywalk.front);
-          xwalk.popFront; ywalk.popFront;
-          xadv = xwalk.direction;
-          yadv = ywalk.direction;
-        } else if (xwalk.front < ywalk.front) {
-          nextT = xwalk.front; xwalk.popFront; xadv = xwalk.direction;
-        } else {
-          assert(xwalk.front > ywalk.front);
-          nextT = ywalk.front; ywalk.popFront; yadv = ywalk.direction;
-        }
-      } else if (!xwalk.empty) {
+private void walkMonoBezier(T, size_t K, Dg)(ref const Point!T[K] curve, Size!T grid, Dg clientDg) {
+  auto xwalk = beziota!("x")(curve, grid.width);
+  auto ywalk = beziota!("y")(curve, grid.height);
+  auto slicer = rollingSlicer(curve, 0.0);
+  auto gridPos = IPoint(xwalk.position, ywalk.position);
+
+  for (bool cont=true; cont;) {
+    double nextT;
+    int xadv, yadv;
+    if (!xwalk.empty && !ywalk.empty) {
+      if (approxEqual(xwalk.front, ywalk.front, 1e-6, 1e-6)) {
+        nextT = 0.5 * (xwalk.front + ywalk.front);
+        xwalk.popFront; ywalk.popFront;
+        xadv = xwalk.direction;
+        yadv = ywalk.direction;
+      } else if (xwalk.front < ywalk.front) {
         nextT = xwalk.front; xwalk.popFront; xadv = xwalk.direction;
-      } else if (!ywalk.empty) {
-        nextT = ywalk.front; ywalk.popFront; yadv = ywalk.direction;
       } else {
-        nextT = 1.0;
-        cont = false;
+        assert(xwalk.front > ywalk.front);
+        nextT = ywalk.front; ywalk.popFront; yadv = ywalk.direction;
       }
-      Point!T[K] slice = void;
-      slicer.advance(nextT, slice);
-      clientDg(gridPos, slice);
-      gridPos.x += xadv; gridPos.y += yadv;
-      assert(gridPos == IPoint(xwalk.position, ywalk.position),
-             to!string(gridPos) ~ "|" ~ to!string(IPoint(xwalk.position, ywalk.position)));
+    } else if (!xwalk.empty) {
+      nextT = xwalk.front; xwalk.popFront; xadv = xwalk.direction;
+    } else if (!ywalk.empty) {
+      nextT = ywalk.front; ywalk.popFront; yadv = ywalk.direction;
+    } else {
+      nextT = 1.0;
+      cont = false;
     }
+    Point!T[K] slice = void;
+    slicer.advance(nextT, slice);
+    clientDg(gridPos, slice);
+    gridPos.x += xadv; gridPos.y += yadv;
+    assert(gridPos == IPoint(xwalk.position, ywalk.position),
+           to!string(gridPos) ~ "|" ~ to!string(IPoint(xwalk.position, ywalk.position)));
   }
 }
 
