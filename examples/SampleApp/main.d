697e78c13fa96c033cfe99fcd3a2e1b18000e3ea
dawg <dawg@dawgfoto.de>
2011-05-22 10:15:54 +0200

    enhance sample application

--- examples/SampleApp/main.d
+++ examples/SampleApp/main.d
@@ -1,32 +1,58 @@
 module SampleApp.main;
 
 private {
-  import skia.core.canvas, skia.views.view2, skia.views.cached, skia.views.layout;
+  import skia.core.canvas, skia.views.view2, skia.views.cached, skia.views.layout, skia.core.paint,
+    skia.views.button, skia.views.cell, skia.views.zoom, skia.views.display, skia.views.select, skia.math.range,
+    skia.core.fonthost._, skia.core.shader;
   import appf.appf, appf.window;
-  import guip.color, guip.event, guip.point, guip.rect, guip.size;
+  import guip.bitmap, guip.color, guip.event, guip.point, guip.rect, guip.size;
+
+//  not yet converted to View2
 //  import SampleApp.bitmapview;
-  import SampleApp.circlesview;
 //  import SampleApp.quadview;
-//  import SampleApp.cubicview;
 //  import SampleApp.sineview;
 //  import SampleApp.rectview;
-//  import SampleApp.textview;
-  import layout.hint;
+
+  import SampleApp.circlesview;
+  import SampleApp.cubicview;
+  import SampleApp.stroke;
+  import SampleApp.svgview;
+  import SampleApp.textview;
+  import SampleApp.pointsview;
+  import std.algorithm, std.array, std.conv, std.traits;
+  import layout.hint, layout.nn;
   import test.utrunner;
+  import std.concurrency;
 }
 
-int main() {
+int main(string[] args) {
   auto app = new AppF();
-  auto handler = new Handler(new WindowView(hbox([new CirclesView, new CirclesView])));
-  auto win1 = app.makeWindow(IRect(IPoint(40, 40), ISize(200, 200)), handler);
+  auto handler = new Handler(new WindowView(thisTid(), buttons(args[1 .. $])));
+  auto win1 = app.makeWindow(IRect(IPoint(40, 40), ISize(1000, 1000)), handler);
   win1.name("Window1");
   win1.show();
-  return app.loop();
+  while (app.dispatch(OnEmpty.Return)) {
+    receiveTimeout(1,
+      (shared BlitEvent e) {
+        auto bmp = cast(Bitmap)e.bmp;
+        IRect area = e.area;
+        win1.blitToWindow(cast(Bitmap)e.bmp, area.pos, area.pos, area.size);
+      },
+      (ResizeEvent e) {
+        win1.resize(e.area.size);
+      });
+  }
+  return 0;
 }
 
+struct BlitEvent {
+  Bitmap bmp;
+  IRect area;
+}
 
 class Handler : WindowHandler {
   WindowView root;
+  Window win;
   ISize size;
 
   this(WindowView root) {
@@ -34,7 +60,7 @@ class Handler : WindowHandler {
   }
 
   override void onEvent(Event e, Window win) {
-    root.win = win;
+    this.win = win;
     visitEvent(e, this);
   }
 
@@ -47,7 +73,7 @@ class Handler : WindowHandler {
     root.onResize(e);
   }
   void visit(RedrawEvent e) {
-    root.win.blitToWindow(root.bmp, e.area.pos, e.area.pos, e.area.size);
+    win.blitToWindow(root.bmp, e.area.pos, e.area.pos, e.area.size);
   }
   void visit(StateEvent e) {
     root.onState(e, size);
@@ -55,50 +81,82 @@ class Handler : WindowHandler {
 }
 
 class WindowView : CachedView {
-  Window win;
-  this(View child) {
+  Tid guiTid;
+
+  this(Tid guiTid, View child) {
+    this.guiTid = guiTid;
     super(child);
   }
 
   override IRect update() {
     auto upd = super.update();
     if (!upd.empty)
-      win.blitToWindow(bmp, upd.pos, upd.pos, upd.size);
+      guiTid.send(cast(shared)BlitEvent(bmp, upd));
     return upd;
   }
 
   override void requestResize(ISize size, View child) {
-    if (win !is null)
-      win.resize(size);
+    guiTid.send(ResizeEvent(IRect(size)));
+  }
+
+  override string lookupAttr(string key) {
+    enum over = [/*"background-color" : "0xFFAA4488"*/, "color" : "0xFF22AA33"];
+    if (auto val = key in over)
+      return *val;
+    else
+      return super.lookupAttr(key);
+  }
+
+  override void onKey(KeyEvent e, ISize size) {
+    if (e.isPress && e.mod.ctrl && e.key.num == 39) {
+      std.stdio.write("saving ");
+      bmp.save("canvas.bmp");
+      std.stdio.writeln("done");
+    }
   }
 }
 
-class Simple : View {
-  this(Color bg) {
-    this.bg = bg;
-  }
-  override void onButton(ButtonEvent e, ISize size) {
-    if (e.button.left && e.isdown)
-      requestResize(size * 2);
-    else if (e.button.right && e.isdown)
-      requestResize(size / 2);
-  }
+View buttons(string[] args) {
+  View[] chs;
+  chs ~= new CellView(new Simple(White));
+  chs ~= new CellView(new Simple(Red));
+  chs ~= new CellView(new Simple(Green));
+  chs ~= new CellView(new Simple(Blue));
+  //  chs ~= new Simple(Orange);
+  //  auto v = new TextView();
+  //  auto v = new CirclesView();
+  auto v = new ZoomView(new CubicView());
+  //  auto v = new SvgView(args.empty ? null : args[0]);
+  //  auto v = new StrokeView();
+  //  auto v = new PointsView();
+  return flexBox([v, flexBox(chs)]);
+}
 
-  override void onMouse(MouseEvent e, ISize size) {
-    requestRedraw(IRect(e.pos, ISize(2, 2)));
+shared uint id;
+class Simple : View {
+  this(Color bg, SizeHint sh=SizeHint(Hint(50, 1.), Hint(50, 10.))) {
+    this.bg = bg;
+    this._sizeHint = sh;
+    this.id = .id++;
   }
 
   override void onDraw(Canvas canvas, IRect area, ISize size) {
-    if (area.size == size)
-      canvas.drawColor(bg);
-    else
-      canvas.drawColor(Black);
+    scope auto paint = new Paint(bg);
+    paint.antiAlias = false;
+    paint.shader = new GradientShader([bg, bg.complement], [FPoint(0, 0), FPoint(size.width, size.height)]);
+    paint.shader.matrix = canvas.curMatrix.inverted;
+
+    canvas.drawPaint(paint);
+    scope auto textPaint = new TextPaint(Black, TypeFace.findFace("DejaVu Sans"));
+    canvas.drawText(to!string(this.id), fPoint(IRect(size).center), textPaint);
+    canvas.drawText("ABA", FPoint(0, size.height), textPaint);
   }
 
   override SizeHint sizeHint() const {
-    return SizeHint(Hint(0, 100, 5000), Hint(0, 100, 5000));
+    return _sizeHint;
   }
 
-  ISize size;
   Color bg;
+  SizeHint _sizeHint;
+  uint id;
 }
