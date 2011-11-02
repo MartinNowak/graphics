e7239a00658dc0f6dd787faa9ebde587ea3e3e64
dawg <dawg@dawgfoto.de>
2011-11-02 07:06:30 +0100

    add stopwatch to example

--- examples/circles.d
+++ examples/circles.d
@@ -1,11 +1,8 @@
 import graphics._, guip._, graphics.core.matrix, graphics.effect.patheffects, graphics.core.shader;
-import std.math, std.stdio;
+import std.datetime, std.math, std.stdio;
 
-void main()
+void paint(Canvas canvas)
 {
-    auto bmp = Bitmap(Bitmap.Config.ARGB_8888, 800, 800);
-
-    scope auto canvas = new Canvas(bmp);
     scope auto paintC = new Paint(Color.Black);
     paintC.fillStyle = Paint.Fill.Stroke;
     paintC.strokeWidth = 1;
@@ -24,6 +21,20 @@ void main()
         canvas.scale(0.999, 0.999);
         canvas.rotate(angle);
     }
+}
 
+void render()
+{
+    auto bmp = Bitmap(Bitmap.Config.ARGB_8888, 800, 800);
+    scope auto canvas = new Canvas(bmp);
+    paint(canvas);
     bmp.save("out.png");
+}
+
+void main()
+{
+    auto sw = StopWatch(AutoStart.yes);
+    render();
+    sw.stop;
+    writeln(sw.peek.msecs);
 }
