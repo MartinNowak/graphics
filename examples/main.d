65e909db38949b0c8685c908bdb61e0462e2a1b2
Martin Nowak <code@dawg.eu>
2014-06-09 18:20:06 +0200

    use minima for test timings

--- examples/main.d
+++ examples/main.d
@@ -1,23 +1,37 @@
 module examples.main;
 
-import std.datetime, std.stdio;
+import std.algorithm, std.datetime, std.stdio, std.typecons;
 import examples.circles, examples.zrusin_path;
+
+Tuple!(ulong, ulong) measure()
+{
+    Tuple!(ulong, ulong) res;
+
+    auto sw = StopWatch(AutoStart.yes);
+    circles("circles.png");
+    res[0] = sw.peek.usecs;
+
+    sw.reset();
+    zrusin_path("zrusin.png");
+    res[1] = sw.peek.usecs;
+
+    return res;
+}
 
 void main()
 {
-    size_t cnt = 2;
+    auto mins = tuple(ulong.max, ulong.max);
 
-    StopWatch sw;
-
-    sw.start;
-    foreach(_; 0 .. cnt)
-        circles("circles.png");
-    sw.stop;
-    writeln("circles ", sw.peek.msecs * 1.0 / cnt);
-    sw.reset;
-    sw.start;
-    foreach(_; 0 .. cnt)
-        zrusin_path("zrusin.png");
-    sw.stop;
-    writeln("zrusin ", sw.peek.msecs * 1.0 / cnt);
+    while (true)
+    {
+        auto vals = measure();
+        vals[0] = min(vals[0], mins[0]);
+        vals[1] = min(vals[1], mins[1]);
+        if (vals != mins)
+        {
+            mins = vals;
+            writef("\rcircles %s us, zrusing %s us", mins[]);
+            stdout.flush();
+        }
+    }
 }
